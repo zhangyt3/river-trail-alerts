4f5d18b2fc1f13c15b452e0f0c2f8297aea62c42
Kenny Zhang <busstopbench@gmail.com>
2019-12-08 12:05:29 -0500

    Add unit test for send_email

--- tests/notifier_test.py
+++ tests/notifier_test.py
@@ -1,6 +1,10 @@
 import pytest
+import os
 
-from notify.notifier import form_email
+from unittest.mock import Mock, patch
+from unittest import mock
+
+from notify.notifier import form_email, send_emails
 
 
 def test_form_email():
@@ -14,4 +18,12 @@ def test_form_email():
     assert len(lines) == 2
     assert 'loc1 changed from closed to open' == lines[0]
     assert 'loc2 changed from open to conditions variable' == lines[1]
+
+def test_send_emails():
+    diffs = [('Nebraska', 'open', 'closed')]
+    sns_client_mock = Mock()
     
+    with mock.patch.dict('os.environ', {'EMAIL_SNS_TOPIC_ARN': 'test-arn'}):
+        send_emails(sns_client_mock, diffs)
+        sns_client_mock.publish.assert_called_once()
+
